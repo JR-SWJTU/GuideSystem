9d4ff5c762009554f6b9f9665cadfb8b56ab3804
JR-SWJTU <752258828@qq.com>
2016-09-29 18:08:34 +0800

    完成登录页面和注册页面，逻辑功能待实现

--- models/User.js
+++ models/User.js
@@ -5,18 +5,19 @@ var mongoose = mongodb.mongoose;
 // 定义一个Schema
 var UserSchema = new mongoose.Schema({
 
-	Student_id : String,
+	Email : String,
+	Name : String,
 	Password : String
 });
 
-UserSchema.index({Student_id:1},{"background" : true});//设置索引
+UserSchema.index({Email:1},{"background" : true});//设置索引
 
 // 将该Schema发布为Entity
 var UserEntity = mongoose.model('UserEntity',UserSchema,'user');//指定在数据库中的collection名称为user
 exports.UserEntity  = UserEntity;//导出UserEntity实体
 
 // var userEntity = new UserModel({
-// 	Student_id : '2014',
+// 	Email : '2014',
 // 	Password : '1123'
 // })
 // console.log(userEntity);
